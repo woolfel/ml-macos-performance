cd72510ecfb0dffaad323ddd3c97cf88ddd05cce
giild <peter@giild.com>
2023-01-26 11:18:50 -0500

    updating the benchmark with a second model to compare performance

--- MLMacosPerf/ViewController.swift
+++ MLMacosPerf/ViewController.swift
@@ -23,20 +23,20 @@ class ViewController: NSViewController {
         DispatchQueue.main.async {
             var summary = "";
             for modelName in [
-                    "densenet121_keras_applications",
+                    "densenet121_keras_applications"
                 ] {
                 for device in ["ANE", "GPU", "CPU"] {
                     // warmup
-                    _ = try! self.runPredictBenchmark(
+                    _ = try! self.runDensenetPredictBenchmark(
                         modelName: modelName,
                         device: device,
-                        numIter: 50
+                        numIter: 100
                     )
                     // real test
-                    let latency = try! self.runPredictBenchmark(
+                    let latency = try! self.runDensenetPredictBenchmark(
                         modelName: modelName,
                         device: device,
-                        numIter: 1000
+                        numIter: 4000
                     )
                     print(modelName)
                     print("Latency \(device) : \(latency)")
@@ -44,7 +44,7 @@ class ViewController: NSViewController {
                     print()
                     summary += "\(modelName)\n"
                     summary += "Latency   \(device) : \(latency)  \n"
-                    summary += "Run/Sec  \(device) : \(1/latency) \n\n"
+                    summary += "RPS  \(device) : \(1/latency) \n\n"
                 }
                 sender.title = "Done!";
             }
@@ -57,11 +57,53 @@ class ViewController: NSViewController {
             self.view.addConstraints([align, width, height]);
         }
     }
-    
+
+    @IBAction func startChickenTestAction(_ sender: NSButton) {
+        sender.title = "Running";
+        if resultSummary != nil {
+            resultSummary?.removeFromSuperview();
+        }
+        DispatchQueue.main.async {
+            var summary = "";
+            for modelName in [
+                    "ChickenDetector"
+                ] {
+                for device in ["ANE", "GPU", "CPU"] {
+                    // warmup
+                    _ = try! self.runChickenPredictBenchmark(
+                        modelName: modelName,
+                        device: device,
+                        numIter: 100
+                    )
+                    // real test
+                    let latency = try! self.runChickenPredictBenchmark(
+                        modelName: modelName,
+                        device: device,
+                        numIter: 4000
+                    )
+                    print(modelName)
+                    print("Latency \(device) : \(latency)")
+                    print("RPS \(device) : \(1 / latency)")
+                    print()
+                    summary += "\(modelName)\n"
+                    summary += "Latency   \(device) : \(latency)  \n"
+                    summary += "RPS  \(device) : \(1/latency) \n\n"
+                }
+                sender.title = "Done!";
+            }
+            self.resultSummary = NSTextField(string: summary);
+            self.view.addSubview(self.resultSummary!);
+            self.resultSummary?.translatesAutoresizingMaskIntoConstraints = false;
+            let align = NSLayoutConstraint(item: self.resultSummary!, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 1);
+            let width = NSLayoutConstraint(item: self.resultSummary!, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 400);
+            let height = NSLayoutConstraint(item: self.resultSummary!, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 245);
+            self.view.addConstraints([align, width, height]);
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.startButton?.title = "Click to Start";
     }
 
     override var representedObject: Any? {
@@ -79,22 +121,24 @@ class ViewController: NSViewController {
         let mlconfig = MLModelConfiguration()
         if (device.lowercased() == "ane") {
             mlconfig.computeUnits = .all
+            mlconfig.allowLowPrecisionAccumulationOnGPU = true
         } else if (device.lowercased() == "gpu") {
             mlconfig.computeUnits = .cpuAndGPU
+            mlconfig.allowLowPrecisionAccumulationOnGPU = true
         } else {
             mlconfig.computeUnits = .cpuOnly
         }
         return mlconfig
     }
     
-    // function will run the benchmark to train
-    func runPredictBenchmark(modelName: String, device: String, numIter: Int = 100) throws -> Double {
+    // function will run the benchmark for DenseNet
+    func runDensenetPredictBenchmark(modelName: String, device: String, numIter: Int = 100) throws -> Double {
         let mlconfig = configureDevice(device: device)
         
         let model = try Predictor(contentsOf: modelName, configuration: mlconfig)
         
         let imageFeatureValue = try MLFeatureValue (
-            imageAt: url(forResource: "test09", withExtension: "jpg"),
+            imageAt: url(forResource: "test03", withExtension: "jpg"),
             constraint: model.mlmodel.modelDescription.inputDescriptionsByName["input_1"]!.imageConstraint!,
             options: [.cropAndScale: VNImageCropAndScaleOption.centerCrop.rawValue]
         )
@@ -109,7 +153,29 @@ class ViewController: NSViewController {
         let elapsed = end - start
         return elapsed / Double(numIter)
     }
-    
+
+    func runChickenPredictBenchmark(modelName: String, device: String, numIter: Int = 100) throws -> Double {
+        let mlconfig = configureDevice(device: device)
+        
+        let model = try Predictor(contentsOf: modelName, configuration: mlconfig)
+        
+        let imageFeatureValue = try MLFeatureValue (
+            imageAt: url(forResource: "chicken", withExtension: "jpg"),
+            constraint: model.mlmodel.modelDescription.inputDescriptionsByName["imagePath"]!.imageConstraint!,
+            options: [.cropAndScale: VNImageCropAndScaleOption.centerCrop.rawValue]
+        )
+        let input = try MLDictionaryFeatureProvider(
+            dictionary: ["imagePath": imageFeatureValue.imageBufferValue!])
+        
+        let start = CACurrentMediaTime()
+        for _ in 0..<numIter {
+            _ = try! model.predict(input: input)
+        }
+        let end = CACurrentMediaTime()
+        let elapsed = end - start
+        return elapsed / Double(numIter)
+    }
+
     func runTrainBenchmark(modelName: String, device: String, withExtension: String) throws {
         let mlconfig = configureDevice(device: device)
 
