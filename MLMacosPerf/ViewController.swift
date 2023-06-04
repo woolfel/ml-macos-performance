ea454e81836a8d29aae9a56921ad1b3cca2012a3
giild <peter@giild.com>
2023-06-04 18:12:40 -0400

    adding rock paper scissor model

--- MLMacosPerf/ViewController.swift
+++ MLMacosPerf/ViewController.swift
@@ -101,6 +101,49 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func startRPSTestAction(_ sender:NSButton) {
+        sender.title = "Running";
+        if resultSummary != nil {
+            resultSummary?.removeFromSuperview();
+        }
+        DispatchQueue.main.async {
+            var summary = "";
+            for modelName in [
+                    "rockpaperscissor_small_wpreview"
+                ] {
+                for device in ["ANE", "GPU", "CPU"] {
+                    // warmup
+                    _ = try! self.runRPSPredict(
+                        modelName: modelName,
+                        device: device,
+                        numIter: 100
+                    )
+                    // real test
+                    let latency = try! self.runRPSPredict(
+                        modelName: modelName,
+                        device: device,
+                        numIter: 4000
+                    )
+                    print(modelName)
+                    print("Latency \(device) : \(latency)")
+                    print("RPS \(device) : \(1 / latency)")
+                    print()
+                    summary += "\(modelName)\n"
+                    summary += "Latency   \(device) : \(latency)  \n"
+                    summary += "RPS  \(device) : \(1/latency) \n\n"
+                }
+                sender.title = "Done!";
+            }
+            self.resultSummary = NSTextField(string: summary);
+            self.view.addSubview(self.resultSummary!);
+            self.resultSummary?.translatesAutoresizingMaskIntoConstraints = false;
+            let align = NSLayoutConstraint(item: self.resultSummary!, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 1);
+            let width = NSLayoutConstraint(item: self.resultSummary!, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 400);
+            let height = NSLayoutConstraint(item: self.resultSummary!, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 245);
+            self.view.addConstraints([align, width, height]);
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -160,7 +203,7 @@ class ViewController: NSViewController {
         let model = try Predictor(contentsOf: modelName, configuration: mlconfig)
         
         let imageFeatureValue = try MLFeatureValue (
-            imageAt: url(forResource: "chicken", withExtension: "jpg"),
+            imageAt: url(forResource: "rock1", withExtension: "jpg"),
             constraint: model.mlmodel.modelDescription.inputDescriptionsByName["imagePath"]!.imageConstraint!,
             options: [.cropAndScale: VNImageCropAndScaleOption.centerCrop.rawValue]
         )
@@ -176,6 +219,28 @@ class ViewController: NSViewController {
         return elapsed / Double(numIter)
     }
 
+    func runRPSPredict(modelName: String, device: String, numIter: Int = 100) throws -> Double {
+        let mlconfig = configureDevice(device: device)
+        
+        let model = try Predictor(contentsOf: modelName, configuration: mlconfig)
+        
+        let imageFeatureValue = try MLFeatureValue (
+            imageAt: url(forResource: "rock1", withExtension: "jpg"),
+            constraint: model.mlmodel.modelDescription.inputDescriptionsByName["conv2d_input"]!.imageConstraint!,
+            options: [.cropAndScale: VNImageCropAndScaleOption.centerCrop.rawValue]
+        )
+        let input = try MLDictionaryFeatureProvider(
+            dictionary: ["conv2d_input": imageFeatureValue.imageBufferValue!])
+        
+        let start = CACurrentMediaTime()
+        for _ in 0..<numIter {
+            _ = try! model.predict(input: input)
+        }
+        let end = CACurrentMediaTime()
+        let elapsed = end - start
+        return elapsed / Double(numIter)
+    }
+    
     func runTrainBenchmark(modelName: String, device: String, withExtension: String) throws {
         let mlconfig = configureDevice(device: device)
 
